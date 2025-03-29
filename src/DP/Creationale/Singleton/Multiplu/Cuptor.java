6caf06806df007e2d3331ad843820aa3017fc768
ANDREEA <cirjanandreea22@stud.ase.ro>
2025-03-30 00:58:36 +0200

    DP creationale Builder Magazin

---
+++ src/DP/Creationale/Singleton/Multiplu/Cuptor.java
@@ -0,0 +1,34 @@
+package DP.Creationale.Singleton.Multiplu;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cuptor {
+	private static Cuptor instance = null;
+	private List<Preparat> listaAsteptare=null;
+	private int gradMaxim;
+
+	private Cuptor(int gradMaxim) {
+		this.listaAsteptare = new ArrayList<>();
+		this.gradMaxim=gradMaxim;
+	}
+	
+public static Cuptor getInstance(int gradMaxim) {
+		if(instance == null) {
+			instance = new Cuptor(gradMaxim);
+		}
+		return instance;
+	}
+
+	public void addPreparat(Preparat p) {
+		if(p!=null) {
+			this.listaAsteptare.add(p);
+		}
+	}
+
+	@Override
+	public String toString() {
+		return "Cuptor [listaAsteptare=" + listaAsteptare + "]";
+	}
+	
+}
